22559635696d5f238256956986c10af837a1a10f
Schrodinger ZHU Yifan <i@zhuyi.fan>
2023-03-15 14:09:09 -0400

    implement fallback hash

--- include/ahash-cxx/hasher.h
+++ include/ahash-cxx/hasher.h
@@ -28,7 +28,7 @@ namespace ahash {
         }
 
         template<typename Accelerator>
-        bool
+        AHASH_CXX_ALWAYS_INLINE bool
         vectorized_consume(const uint8_t *data, size_t length) {
             const auto duplicated_key = Accelerator::broadcast_from(key);
             const size_t num_lanes = Accelerator::lanes(duplicated_key);
@@ -110,7 +110,7 @@ namespace ahash {
             return VectorOperator::lower_half(result);
         }
 
-        void
+        AHASH_CXX_ALWAYS_INLINE void
         consume(const void *__restrict input, size_t length) {
             const auto *data = reinterpret_cast<const uint8_t *> (input);
             enc = VectorOperator::add_extra_data(enc, length);
@@ -154,4 +154,113 @@ namespace ahash {
 
 #endif
 
+    template<uint64_t MULTIPLE, uint64_t ROT>
+    class FallbackHasher {
+        uint64_t buffer;
+        uint64_t pad;
+        uint64_t extra_keys[2]{};
+
+        static uint64_t folded_multiply(uint64_t x, uint64_t y) {
+            auto a = static_cast<unsigned __int128>(x);
+            auto b = static_cast<unsigned __int128>(y);
+            auto result = a * b;
+            auto low = static_cast<uint64_t>(result);
+            auto high = static_cast<uint64_t>(result >> 64);
+            return low ^ high;
+        }
+
+        static uint64_t rotate_left(uint64_t x, uint64_t y) {
+#pragma push_macro("__has_builtin")
+#ifndef __has_builtin
+#define __has_builtin(...) 0
+#endif
+#if __has_builtin(__builtin_rotateleft64)
+            return __builtin_rotateleft64(x, y);
+#else
+            return (x << y) | (x >> (64 - y));
+#endif
+#pragma pop_macro("__has_builtin")
+        }
+
+
+        void consume(uint64_t low_addr, uint64_t high_addr) {
+            uint64_t low = low_addr;
+            uint64_t high = high_addr;
+#if __BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__
+            uint64_t tmp = low;
+            low = high;
+            high = tmp;
+#endif
+            auto combined = folded_multiply(low ^ extra_keys[0], high ^ extra_keys[1]);
+            buffer = rotate_left((buffer + pad) ^ combined, ROT);
+        }
+
+        void consume(unsigned __int128 t) {
+            auto low = static_cast<uint64_t>(t);
+            auto high = static_cast<uint64_t>(t >> 64);
+            auto combined = folded_multiply(low ^ extra_keys[0], high ^ extra_keys[1]);
+            buffer = rotate_left((buffer + pad) ^ combined, ROT);
+        }
+
+    public:
+        explicit FallbackHasher(uint64_t seed) {
+            buffer = PI[0];
+            pad = PI[1];
+            extra_keys[0] = PI[2];
+            extra_keys[1] = PI[3];
+            consume(seed);
+            auto mix = [this](uint64_t x, uint64_t y) {
+                FallbackHasher h = *this;
+                h.consume(x);
+                h.consume(y);
+                return h.finalize();
+            };
+            uint64_t S[] = {mix(PI2[0], PI2[2]), mix(PI2[1], PI2[3]),
+                            mix(PI2[2], PI2[1]), mix(PI2[3], PI2[0])};
+            buffer = S[0];
+            pad = S[1];
+            extra_keys[0] = S[2];
+            extra_keys[1] = S[3];
+        }
+
+        void
+        consume(const void *__restrict input, size_t length) {
+            const auto *data = reinterpret_cast<const uint8_t *> (input);
+            buffer = (buffer + length) * MULTIPLE;
+
+            if (length > 8) {
+                if (length > 16) {
+                    const auto tail = generic_load<unsigned __int128>(&data[length - 16]);
+                    consume(tail);
+                    while (length > 16) {
+                        const auto head = generic_load<unsigned __int128>(data);
+                        consume(head);
+                        data += 16;
+                        length -= 16;
+                    }
+                } else {
+                    const auto x = generic_load<uint64_t>(&data[0]);
+                    const auto y = generic_load<uint64_t>(&data[length - 8]);
+                    consume(x, y);
+                }
+            } else {
+                const auto value = SmallData::load(data, length);
+                consume(value.data[0], value.data[1]);
+            }
+        }
+
+        [[nodiscard]] uint64_t
+        finalize() const {
+            const auto rot = buffer & 63;
+            return rotate_left(folded_multiply(buffer, pad), rot);
+        }
+    };
+
+    constexpr uint64_t DEFAULT_MULTIPLE = 6364136223846793005;
+    constexpr uint64_t DEFAULT_ROT = 23;
+#if AHASH_CXX_HAS_BASIC_SIMD_ACCELERATION
+    using Hasher = VectorizedHasher;
+#else
+    using Hasher = FallbackHasher<DEFAULT_MULTIPLE, DEFAULT_ROT>;
+#endif
 }
